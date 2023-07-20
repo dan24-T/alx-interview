f350b0720150b769b32ec51e6c4bb9776611c9cb
dan24-T <106978779+dan24-T@users.noreply.github.com>
2023-07-21 02:14:30 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,23 +1,53 @@
 #!/usr/bin/python3
-"""
-Function pascal triangle
-"""
+import sys
 
 
-def pascal_triangle(n):
-    """Print Pascal's Triangle
-        n (int): Size of the pascal triangle
+def print_msg(dict_sc, total_file_size):
     """
-    t = []
-    if (n <= 0):
-        return t
-    else:
-        for i in range(n+1):
-            p = []
-            c = 1
-            for j in range(1, i+1):
-                p.append(c)
-                c = c * (i - j) // j
-            if (len(p)):
-                t.append(p)
-    return t
+    Method to print
+    Args:
+        dict_sc: dict of status codes
+        total_file_size: total of the file
+    Returns:
+        Nothing
+    """
+
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
+
+
+total_file_size = 0
+code = 0
+counter = 0
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
+
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
+
+        if len(parsed_line) > 2:
+            counter += 1
+
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
+
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
+
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
+
+finally:
+    print_msg(dict_sc, total_file_size)
