1a04fdd0c19c5a9cc7615bc0c1059d3f3f0178f3
dan24-T <106978779+dan24-T@users.noreply.github.com>
2023-09-08 00:24:51 +0300

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
+"""Prime game.
+"""
 
-""" makeChange"""
 
-
-def makeChange(coins, total):
+def isWinner(x, nums):
+    """Determines the winner.
     """
-    Returns: fewest number of coins needed to meet
-    """
-    if not coins or coins is None:
-        return -1
-    if total <= 0:
-        return 0
-    change = 0
-    coins = sorted(coins)[::-1]
-    for coin in coins:
-        while coin <= total:
-            total -= coin
-            change += 1
-        if (total == 0):
-            return change
-    return -1
+    if x < 1 or not nums:
+        return None
+    marias_wins, bens_wins = 0, 0
+    # generate primes
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    # filter
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+    if marias_wins == bens_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
